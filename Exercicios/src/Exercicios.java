54d99b5c1487d79f702dacbc54a4179aec72630f
RafaelaDiniz <rafaela2106@hotmail.com>
2022-11-09 15:27:07 -0300

    up

---
+++ Exercicios/src/Exercicios.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/* Faça um programa que leia conjuntos de dois valores,
+o primeiro representando o nome do aluno e o segundo representando a sua idade.
+(Pare o programa inserindo o valor 0 no campo nome) */ 
+
+public class Exercicios {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+        while (true) {
+            System.out.println("nome : ");
+            nome = scanner.next();
+            if (nome.equals("0"))break;
+
+            System.out.println("Idade:");
+            idade = scanner.nextInt();
+        }
+        System.out.println("continua aqui...");
+        }
+    }
---
+++ Exercicios/src/Nota.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Nota {
+    /* Faça um programa que peça uma nota entre 0 e 10.
+     * mostre uma mensagem caso o valor seja invalido e continue pedindo.
+     * até que o usuário informe um valor válido
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int nota;
+
+        System.out.println("nota: ");
+        nota = scanner.nextInt();
+
+        while (nota < 0 | nota > 10){
+            System.out.println("nota inválida ! Digite nota novamente");
+            nota = scanner.nextInt();
+            
+        }
+    }
+}
