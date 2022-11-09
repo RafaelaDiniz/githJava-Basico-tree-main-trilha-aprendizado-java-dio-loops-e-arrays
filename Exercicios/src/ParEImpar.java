1cbd1f76833f3533a842e2c8197a587fae581557
RafaelaDiniz <rafaela2106@hotmail.com>
2022-11-09 16:36:09 -0300

    up

---
+++ Exercicios/src/ParEImpar.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class ParEImpar {
+
+    public static void main(String[] args) {
+        /*
+    Faça um programa que peça N números inteiros,
+    calcule e mostre a quantidade de números pares
+    e a quantidade de números impares.
+    */
+    Scanner scanner = new Scanner(System.in);
+    int numero;
+    int quantNumeros;
+    int quantPares = 0;
+    int quantImpares = 0;
+
+    System.out.println(" Quantidade de números: ");
+    quantNumeros = scanner.nextInt();
+    int count = 0;
+    do {
+        System.out.println("numero: ");
+        numero= scanner.nextInt();
+
+    
+    if (numero % 2 == 0) quantPares++;
+    else quantImpares++;
+
+
+    count++;
+    } 
+    while (count < quantNumeros);
+
+    System.out.println("Quantidade pares:" + quantPares);
+    System.out.println("Quantidade ímpares: " + quantImpares);
+
+    }       
+}
+
+
